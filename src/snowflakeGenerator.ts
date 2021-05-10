1e1768c8f5b8fd0f1fe2d91253e4b6e515612aa2
johnnyracket <john.randolph.harrison@gmail.com>
2021-05-10 11:28:55 -0500

    targeting newer es2020 to use bigint literals

--- src/snowflakeGenerator.ts
+++ src/snowflakeGenerator.ts
@@ -1,9 +1,9 @@
 function* snowflakeGenerator(machineId: number) {
-  let seq = BigInt(0);
+  let seq = 0n;
   // shift time 22 bits left to occupy first 42 bits of 64
-  const shiftLeftTime = BigInt(22);
+  const shiftLeftTime = 22n;
   // get machine id: 10 bit int to enable distributed keys being unique, shift 12 bits left to occupy bits 42-52
-  const machine = (BigInt(machineId) << BigInt(12)) ;
+  const machine = BigInt(machineId) << 12n;
   let lastTime = BigInt(Date.now());
   while (true) {
     try {
@@ -11,12 +11,13 @@ function* snowflakeGenerator(machineId: number) {
       seq++;
       // get current datetime in millliseconds
       const time = BigInt(Date.now());
+      const shiftedTime = time << shiftLeftTime;
       // if we are in the same millsecond, but we have maxed out our sequence we cannot vend a unique id (wrap at 12 bits)
-      if (time === lastTime && seq >= 4096) throw Error('Exceeded 4096 ids in 1 millisecond.')
+      if (time === lastTime && seq >= 4096n) throw Error('Exceeded 4096 ids in 1 millisecond.')
       // if we are on a new millisecond reset the seq
-      if (time !== lastTime) seq = BigInt(0);
+      if (time !== lastTime) seq = 0n;
       // write 64 bits into an unsigned BigInt: 42bits time, 10 bits machine, 12 bits seq. 
-      const id = BigInt.asUintN(64, time << shiftLeftTime | machine | seq);
+      const id = BigInt.asUintN(64, shiftedTime | machine | seq);
       lastTime = time;
       yield id.toString();
     } catch (e) {
