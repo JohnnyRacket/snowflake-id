478cfb68445ccfbf1b69634ff7c61314ef6f1de8
John Harrison <john.randolph.harrison@gmail.com>
2021-05-10 10:45:02 -0500

    removed unecessary buffer, and fixed logic around ensuring uniqueness

--- src/snowflakeGenerator.ts
+++ src/snowflakeGenerator.ts
@@ -2,8 +2,6 @@ function* snowflakeGenerator(machineId: number) {
   let seq = 0;
   // get machine id: 10 bit int to enable distributed keys being unique
   let machine = machineId;
-  // allocate a 64 bit (8 byte) buffer
-  let id = Buffer.alloc(8);
   let lastTime = Date.now();
   while (true) {
     try {
@@ -12,17 +10,13 @@ function* snowflakeGenerator(machineId: number) {
       // get current datetime in millliseconds
       let time = Date.now();
       // if we are in the same millsecond, but we have maxed out our sequence we cannot vend a unique id (wrap at 12 bits)
-      if (time !== lastTime && seq >= 4096) throw Error('Exceeded 4096 ids in 1 millisecond.') 
+      if (time === lastTime && seq >= 4096) throw Error('Exceeded 4096 ids in 1 millisecond.')
       // if we are on a new millisecond reset the seq
       if (time !== lastTime) seq = 0;
-      // fill buffer with 0's
-      id.fill(0);
-      // write 64 bits of the buffer: 42bits time, 10 bits machine, 12 bits seq. clamped as unsigned int
-      id.writeBigUInt64BE(BigInt.asUintN(64, BigInt(time) << BigInt(22) | (BigInt(machine) << BigInt(12)) | BigInt(seq)));
-      // convert from byte buffer to 64bit unsigned int
-      const bigint = id.readBigUInt64BE();
+      // write 64 bits into an unsigned BigInt: 42bits time, 10 bits machine, 12 bits seq. 
+      const id = BigInt.asUintN(64, BigInt(time) << BigInt(22) | (BigInt(machine) << BigInt(12)) | BigInt(seq));
       lastTime = time;
-      yield bigint.toString();
+      yield id.toString();
     } catch (e) {
       console.log(e);
       yield new Error('Failed to generate snowflake id.');
